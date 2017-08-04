ae1f7c1da2145ac8338a3cc0688765d3c8a959dd
Stanleyterhaar <30719260+Stanleyterhaar@users.noreply.github.com>
2017-08-04 14:08:42 +0200

    Add files via upload

---
+++ version.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+var (
+	GitCommit   string
+	VersionDesc string
+)
+
+const (
+	Version = "2.3.0-pre"
+)
+
+func init() {
+	gitCommit := ""
+	if len(GitCommit) > 0 {
+		gitCommit = "; git " + GitCommit
+	}
+	VersionDesc = fmt.Sprintf("git-lfs/%s (GitHub; %s %s; go %s%s)",
+		Version,
+		runtime.GOOS,
+		runtime.GOARCH,
+		strings.Replace(runtime.Version(), "go", "", 1),
+		gitCommit,
+	)
+}
